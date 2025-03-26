e414545201db14b897d852db3ff544130fb95870
Asif Ahmad Khan <97660001+asifkhan03@users.noreply.github.com>
2025-03-26 11:05:40 +0530

    Create AWS-cli.sh

---
+++ AWS-cli.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Download the AWS CLI version 2 installer package
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Install the unzip package
+sudo apt-get install unzip -y
+
+# Unzip the AWS CLI installer package
+unzip awscliv2.zip
+
+# Run the AWS CLI install program
+sudo ./aws/install
