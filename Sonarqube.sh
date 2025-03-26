ce615520aad8dae4cbf7721f816ba6c163ac0f27
Asif Ahmad Khan <97660001+asifkhan03@users.noreply.github.com>
2025-03-26 11:10:26 +0530

    Update Sonarqube.sh

--- Sonarqube.sh
+++ Sonarqube.sh
@@ -1,1 +1,11 @@
- docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+# 1. install docker & INSTALL SONARQUBE AS A CONTAINER IN THE EC2 INSTANCE
+
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sleep 5
+sudo usermod -aG docker ubuntu 
+newgrp docker
+sleep 25
+sudo chmod 777 /var/run/docker.sock
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+sleep 5
